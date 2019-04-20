4ace5a96e5353b4ccc96dad6adaf9daedb2383d7
Marco Oman <marco.oman69@gmail.com>
2019-04-20 19:10:15 +0200

    Compiles, but does not runs correctly

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <tuple>
 
 namespace zn
 {
@@ -7,6 +8,37 @@ namespace zn
     {
         static T module(void) { return N ; }
     } ;
+    
+    template <class T>
+    T gcd(T a, T b)
+    {
+        while (a)
+        {
+            T c = b % a ;
+            b = a ;
+            a = c ;
+        }
+        return b ;
+    }
+    
+    template <class T>
+    std::tuple<T, T, T> extended_euclidean_algorithm(const T &a, const T &b)
+    {
+        T q ;
+        int i, j ;
+        T r[2] = {a, b} ;
+        T s[2] = {1, 0} ;
+        T t[2] = {0, 1} ;
+        
+        for (i = 0, j = 1 ; r[j] ; i=j, j = (j + 1) & 1)
+        {
+            q = r[i] / r[j] ;
+            r[i] = r[j] - q * r[i] ;
+            s[i] = s[j] - q * s[i] ;
+            t[i] = t[j] - q * t[i] ;
+        }
+        return std::tuple<T, T, T>(r[i], s[i], t[i]) ;
+    }
     //
     // class for modular arithmetic
     //
@@ -52,6 +84,21 @@ namespace zn
             result += M::module() ;
         return zn_t<T, M>(result) ;
     }
+    
+    template <class T, class M>
+    zn_t<T, M> operator*(const zn_t<T, M> &lhs, const zn_t<T, M> &rhs)
+    {
+        return zn_t<T, M>((lhs.value() * rhs.value()) % M::module()) ;
+    }
+
+    template <class T, class M>
+    zn_t<T, M> operator/(const zn_t<T, M> &lhs, const zn_t<T, M> &rhs)
+    {
+        auto result = extended_euclidean_algorithm(M::module(), rhs.value()) ;
+        if (std::get<0>(result) != 1)
+            throw std::runtime_error("") ;
+        return zn_t<T, M>((lhs.value() * std::get<1>(result)) % M::module()) ;
+    }
 }
 
 using namespace zn ;
@@ -62,4 +109,9 @@ int main(void)
     
     z17_t a(15), b(7) ;
     std::cout << (a + b).value() << std::endl ;
+    std::cout << (a - b).value() << std::endl ;
+    std::cout << (a * b).value() << std::endl ;
+    std::cout << (a / b).value() << std::endl ;
+    std::cout << gcd(27, 84) << std::endl ;
+    return 0 ;
 }
