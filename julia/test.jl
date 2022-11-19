9216d2e4c72fd75e43ca0976935c4540cacd18fd
Marco Oman <marco.oman69@gmail.com>
2022-11-19 07:57:17 +0100

    Added a test

---
+++ julia/test.jl
@@ -0,0 +1,34 @@
+#
+# Testing script
+# Here some primes used for tests
+# 79 223 557 12377 2309 3109 3461 5107 5857 6571 7297
+#
+using Printf
+include("eratostenes.jl")
+
+function test_quadratic_residue(limit)
+    primes = eratosthenes_sieve(100)
+    errors = 0
+    for p in primes
+        for n =1:p-1
+            r = quadratic_residue_odd(n, p)
+            # do the same 'by hand'
+            r1 = 0
+            for j=1:p÷2
+                if j*j%p == n
+                    r1 = j
+                    break
+                end
+            end
+            if (r1 != r) && (r1 != p-r)
+                @printf("Error on %d: %d instead of %d\n", p, r, r1)
+                errors += 1
+            end
+        end
+    end
+    return errors ;
+end
+
+
+err = test_quadratic_residue(100)
+@printf("tested quadratic residue, found %d errors\n", err)
